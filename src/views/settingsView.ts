e07088e9c5e040a97fd7e0f409fba1e2f5d30e83
chockmimura <avmimurajr@gmail.com>
2025-01-24 19:21:48 +0800

    GDLE: Main page UI revamp

--- src/views/settingsView.ts
+++ src/views/settingsView.ts
@@ -1,25 +1,32 @@
 export function settingsView(settingsPage: HTMLElement, mainPage: HTMLElement) {
     const settingsBtn = document.getElementById("settings-btn") as HTMLButtonElement;
+    const quitBtn = document.getElementById("quit-btn") as HTMLButtonElement;
     const backBtn = document.getElementById("settings-back-btn") as HTMLButtonElement;
     const menuButtons = document.querySelectorAll(".menu-btn") as NodeListOf<HTMLButtonElement>;
     const goodleText = document.getElementById("goodle-text");
 
-    // Open Settings
     settingsBtn.addEventListener("click", () => {
+        // Control pages UI
         settingsPage.classList.remove("translate-x-full");
         mainPage.classList.remove("hidden");
         mainPage.classList.add("translate-x-[-40%]"); // Shift main-page buttons
         mainPage.classList.remove("xs:opacity-100");
+
+        // Control buttons and text UI
         menuButtons.forEach((btn) => btn.disabled = true);
+        quitBtn.classList.add("hidden");
+        settingsBtn.classList.add("hidden");
         goodleText?.classList.add("opacity-50");
     });
 
-    // Close Settings
     backBtn.addEventListener("click", () => {
         settingsPage.classList.add("translate-x-full");
         mainPage.classList.remove("translate-x-[-40%]"); 
         mainPage.classList.add("xs:opacity-100");
+
         menuButtons.forEach((btn) => btn.disabled = false);
+        quitBtn.classList.remove("hidden");
+        settingsBtn.classList.remove("hidden");
         goodleText?.classList.remove("opacity-50");
     });
 }
